264bda5ceca7e6f805ff30ae9aa0b9854f7cc4ee
Jelena Savic <76621162+jelenasavic123@users.noreply.github.com>
2024-08-21 11:36:18 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,16 +5,16 @@ const urlsToCache = [
   '/styles/main.css',
   '/scripts/main.js',
   '/p/lista-svih-serija_5.html',  // Dodajte sve URL-ove koje želite da keširate
-  'https://jelenasavic123.github.io/my-blog-assets/slike/16.png',
-  'https://jelenasavic123.github.io/my-blog-assets/slike/32.png',
-  'https://jelenasavic123.github.io/my-blog-assets/slike/48.png',
-  'https://jelenasavic123.github.io/my-blog-assets/slike/64.png',
-  'https://jelenasavic123.github.io/my-blog-assets/slike/128.png',
-  'https://jelenasavic123.github.io/my-blog-assets/slike/192.png',
-  'https://jelenasavic123.github.io/my-blog-assets/slike/360.png',
-  'https://jelenasavic123.github.io/my-blog-assets/slike/512.png',
-  'https://jelenasavic123.github.io/my-blog-assets/slike/screenshot1.png',
-  'https://jelenasavic123.github.io/my-blog-assets/slike/screenshot2.png'
+  'https://jelenasavic123.github.io/my-blog-assets/16.png',
+  'https://jelenasavic123.github.io/my-blog-assets/32.png',
+  'https://jelenasavic123.github.io/my-blog-assets/48.png',
+  'https://jelenasavic123.github.io/my-blog-assets/64.png',
+  'https://jelenasavic123.github.io/my-blog-assets/128.png',
+  'https://jelenasavic123.github.io/my-blog-assets/192.png',
+  'https://jelenasavic123.github.io/my-blog-assets/360.png',
+  'https://jelenasavic123.github.io/my-blog-assets/512.png',
+  'https://jelenasavic123.github.io/my-blog-assets/screenshot1.png',
+  'https://jelenasavic123.github.io/my-blog-assets/screenshot2.png'
 ];
 
 // Instalirajte service worker i keširajte stranice
