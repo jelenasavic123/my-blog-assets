2ff9a81d56556b011081b61ea2ce31868dbdab90
Jelena Savic <76621162+jelenasavic123@users.noreply.github.com>
2024-08-21 12:27:04 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ const urlsToCache = [
   '/index.html',
   '/styles/main.css',
   '/scripts/main.js',
-  '/p/lista-svih-serija_5.html',  // Dodajte sve URL-ove koje želite da keširate
+  '/p/lista-svih-serija_5.html',
   'https://jelenasavic123.github.io/my-blog-assets/16.png',
   'https://jelenasavic123.github.io/my-blog-assets/32.png',
   'https://jelenasavic123.github.io/my-blog-assets/48.png',
@@ -15,10 +15,9 @@ const urlsToCache = [
   'https://jelenasavic123.github.io/my-blog-assets/512.png',
   'https://jelenasavic123.github.io/my-blog-assets/screen360x640.png',
   'https://jelenasavic123.github.io/my-blog-assets/screen360x640-2.png'
-  
 ];
 
-// Instalirajte service worker i keširajte stranice
+// Instalirajte servisni radnik i keširajte stranice
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -27,10 +26,28 @@ self.addEventListener('install', (event) => {
   );
 });
 
+// Aktivirajte novi servisni radnik i očistite stare keševe
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            // Brisanje starih keševa
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
 // Poslušajte fetch događaje i vratite keširane resurse ili mrežne odgovore
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
+      // Vraća keširani odgovor ako postoji, inače preuzima sa mreže
       return response || fetch(event.request);
     })
   );
