0c21a8fc877825d4608879b120d7a2d6754e73b4
Jelena Savic <76621162+jelenasavic123@users.noreply.github.com>
2024-08-21 14:17:58 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,10 +5,21 @@ const urlsToCache = [
   '/styles/main.css',
   '/scripts/main.js',
   '/p/lista-svih-serija_5.html',
-  '/p/nasa-preporuka.html',  // Dodajte sve relevantne stranice
-  // Dodajte sve druge stranice i resurse koje želite keširati
+  '/p/nasa-preporuka.html',
+  '/p/najgedanije.html',  // Dodajte ovu stranicu
+  '/2024/05/laz-yalan.html',  // Dodajte ovaj post
   'https://jelenasavic123.github.io/my-blog-assets/16.png',
-  // Dodajte sve ikone i slike
+  'https://jelenasavic123.github.io/my-blog-assets/32.png',
+  'https://jelenasavic123.github.io/my-blog-assets/48.png',
+  'https://jelenasavic123.github.io/my-blog-assets/64.png',
+  'https://jelenasavic123.github.io/my-blog-assets/128.png',
+  'https://jelenasavic123.github.io/my-blog-assets/192.png',
+  'https://jelenasavic123.github.io/my-blog-assets/360.png',
+  'https://jelenasavic123.github.io/my-blog-assets/512.png',
+  'https://jelenasavic123.github.io/my-blog-assets/screen360x640.png',
+  'https://jelenasavic123.github.io/my-blog-assets/1280x800.png',
+  'https://jelenasavic123.github.io/my-blog-assets/screen360x640-2.png',
+  '/offline.html'  // Dodajte ovu stranicu za offline prikaz
 ];
 
 // Instalirajte servisni radnik i keširajte stranice
@@ -28,6 +39,7 @@ self.addEventListener('activate', (event) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
+            // Brisanje starih keševa
             return caches.delete(cacheName);
           }
         })
