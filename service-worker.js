e2348c331ef32fca9ae4d79c956595acba28b635
Jelena Savic <76621162+jelenasavic123@users.noreply.github.com>
2024-08-21 09:34:01 +0200

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'my-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles/main.css',
+  '/scripts/main.js',
+  '/p/lista-svih-serija_5.html',  // Dodajte sve URL-ove koje želite da keširate
+  // Dodajte ostale URL-ove stranica koje želite da keširate
+];
+
+// Instalirajte service worker i keširajte stranice
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Poslušajte fetch događaje i vratite keširane resurse ili mrežne odgovore
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
